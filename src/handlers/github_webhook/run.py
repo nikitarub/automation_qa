6c69c4a4c405d679b542e984a4e92899c91c8d30
nikitarub <nikita.rub@yandex.ru>
2023-11-14 19:25:54 +0300

    fixed webhook host adress

--- src/handlers/github_webhook/run.py
+++ src/handlers/github_webhook/run.py
@@ -4,6 +4,6 @@ import asyncio
 
 
 async def run_webhook():
-    config = uvicorn.Config(app, port=3000, log_level="info")
+    config = uvicorn.Config(app, port=3000, host="0.0.0.0", log_level="info")
     server = uvicorn.Server(config)
     await server.serve()
